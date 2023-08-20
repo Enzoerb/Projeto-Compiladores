1720163e070b8b9b44b0d1e2ab7439fb2edf40c3
Luan Acarino <Luan Fazito>
2023-08-19 22:14:03 -0300

    feat Implementando comando de repeticao While + ajuste de estrutura do projeto

---
+++ IsiLanguageEmbriaoMaster/src/br/com/professorisidro/isiLanguage/ast/AbstractCommand.java
@@ -0,0 +1,8 @@
+package IsiLanguageEmbriaoMaster.src.br.com.professorisidro.isiLanguage.ast;
+
+public abstract class AbstractCommand {
+
+	
+	public abstract String generateJavaCode();
+	//public abstract String generateGolangCode();
+}
---
+++ IsiLanguageEmbriaoMaster/src/br/com/professorisidro/isiLanguage/exceptions/IsiSemanticException.java
@@ -0,0 +1,8 @@
+package IsiLanguageEmbriaoMaster.src.br.com.professorisidro.isiLanguage.exceptions;
+
+public class IsiSemanticException extends RuntimeException{
+	public IsiSemanticException(String msg) {
+		super(msg);
+	}
+
+}
